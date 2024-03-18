e112e85cfad268b76d9ce7b91a3a44594213c2e3
c4ei <his001_ccp@naver.com>
2024-03-18 13:22:13 +0900

    ing

---
+++ server.js
@@ -0,0 +1,21 @@
+// npm init -y 
+// npm install express cors
+// node server.js
+
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.json());
+var cors = require('cors');
+app.use(cors());
+
+
+app.use('/', express.static( path.join(__dirname, 'front-end/build') ))
+app.get('/', function(req,resp){
+  resp.sendFile( path.join(__dirname, 'front-end/build/index.html') )
+}) 
+
+
+app.listen(3013, function () {
+  console.log('listening on 3013')
+}); 
