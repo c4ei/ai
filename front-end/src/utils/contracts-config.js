c198f5663053edf035da095daf10a67a72eec084
c4ei <his001_ccp@naver.com>
2024-03-20 21:17:53 +0900

    upd

--- front-end/src/utils/contracts-config.js
+++ front-end/src/utils/contracts-config.js
@@ -1,6 +1,6 @@
 
-      export const marketContractAddress = "0x178c3381Ad558581CE686090acE41e98395376df"
-      export const nftContractAddress = "0x98cf6d65102A3F85EAe07aaBD5c3a52C6aA294Be"
-      export const artistsContractAddress = "0xe66cd3F51408F6167321Bce0769263478BAD6800"
+      export const marketContractAddress = "0xa43657aC09932f97169e2235B332B650741b4A7C"
+      export const nftContractAddress = "0x2BAa153BDB8FD8cB262e6581fA88dd76DFc8c2e9"
+      export const artistsContractAddress = "0xAd99Cd5E3ef152F179C0483E2E4a93F280d09C7B"
       export const networkDeployedTo = "21133"
-      export const mockDAIAddress = "0xA0001F4636bE1249A324C855a167d31d20eC0D7E"
+      export const mockDAIAddress = "0x3adBF8fa04c21517D8E50908305D0413D2A44300"
